ec2f5749264d307afbce20e89caa27686e4fc379
jun7054 <dhakla3@gmail.com>
2024-06-03 21:17:07 +0900

    6_3

---
+++ 6_3/app.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import {Component} from 'react';
+import {MyComponent} from './MyComponent'
+
+
+//함수 컴포넌트 선언
+function App() {
+  return <MyComponent/>;
+}
+
+// 클래스형 컴포넌트
+// state 기능을 사용할수가 있다.
+// class App extends Component{
+//   render() {
+//     const name = 'react';
+//     return <div className='react'>{name}</div>;
+//   }
+// }
+export default App;
